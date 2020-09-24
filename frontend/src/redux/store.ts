a389ffdafedf76b0f9e81ad08f780b30809ae5dd
AnyaSen <anyasenchikhina18@gmail.com>
2020-09-24 17:51:16 +0300

    Added redux devtools extension

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -9,4 +9,8 @@ const reducers = combineReducers({
 
 export type IAppState = ReturnType<typeof reducers>;
 
-export const store = createStore(reducers);
+export const store = createStore(
+  reducers,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+);
