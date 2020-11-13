b022e1139b0d6b73139432f12c722b70222cbe43
AnyaSen <anyasenchikhina18@gmail.com>
2020-11-13 11:23:47 +0200

    Made changes for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const RecipeRouter = require("./routes/recipe-router");
+const path = require("path");
 
 const app = express();
 
@@ -8,6 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(RecipeRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
