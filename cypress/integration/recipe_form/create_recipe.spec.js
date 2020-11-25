77538e8c8edd9233c86a7db4495fe236c687cfec
AnyaSen <anyasenchikhina18@gmail.com>
2020-11-25 11:23:11 +0200

    Fixed a test spec

--- cypress/integration/recipe_form/create_recipe.spec.js
+++ cypress/integration/recipe_form/create_recipe.spec.js
@@ -1,21 +1,21 @@
 describe("RecipeForm", () => {
   const name = "test";
 
-  // it("Creates a recipe", () => {
-  //   cy.visit("http://localhost:3000/create");
+  it("Creates a recipe", () => {
+    cy.visit("http://localhost:3000/create");
 
-  //   cy.fillRecipeForm(name);
-  //   cy.contains("Continue").click();
-  //   cy.contains("Submit").click();
-  //   cy.contains("Your recipe has been successfully uploaded!").should(
-  //     "be.visible"
-  //   );
-  //   cy.contains("To the recipes").click();
-  //   cy.get(`[data-cy=${name}]`)
-  //     .children()
-  //     .should("contain", name)
-  //     .and("be.visible");
-  // });
+    cy.fillRecipeForm(name);
+    cy.contains("Continue").click();
+    cy.contains("Submit").click();
+    cy.contains("Your recipe has been successfully uploaded!").should(
+      "be.visible"
+    );
+    cy.contains("To the recipes").click();
+    cy.get(`[data-cy=${name}]`)
+      .children()
+      .should("contain", name)
+      .and("be.visible");
+  });
 
   it("Shows the correct information about the recipe", () => {
     const time = "10min";
